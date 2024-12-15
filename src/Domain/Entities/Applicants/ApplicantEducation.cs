4f1dac2345e16a1936b932f6343923abd0960d27
salman <salman.shafiq@asdbd.com>
2024-12-15 19:26:48 +0600

    model changed

--- src/Domain/Entities/Applicants/ApplicantEducation.cs
+++ src/Domain/Entities/Applicants/ApplicantEducation.cs
@@ -2,13 +2,17 @@
 
 public class ApplicantEducation: BaseAuditableEntity
 {
-    public int EducationLevel { get; set; }
+    public Guid EducationLevel { get; set; }
     public string InstituteName { get; set; }
     public string CountryOfInstitute { get; set; }
     public string MajorSubject { get; set; }
     public int PassingYear { get; set; }
+    public int ResultType { get; set; }
     public string Result { get; set; }
+    public decimal ResultScale { get; set; }
     public Guid ApplicantId { get; set; }
+    public string Board { get; set; }
+    public bool IsHeighestEducation { get; set; }
 
     public virtual Applicant Applicant { get; set; } = default!;
 }
